b60c7f6c02f4ec7de98f5cee44f3919b46080bc2
raduxc44 <raduxc44@gmail.com>
2022-03-25 23:08:03 +0200

    The buttons now display on the result screen

--- script.js
+++ script.js
@@ -18,7 +18,33 @@ const operate = (operator, a, b) => {
             break;
     }
 }
-const numberButtons = document.querySelectorAll('.number');
+const numberButtons = Array.from(document.querySelectorAll('.number'));
 const display = document.querySelector('.display-value');
-display.innerText = 0;
-const numberOutput = (num) => { display.innerText += num }
+const buttonOne = document.querySelector('.one')
+const buttonTwo = document.querySelector('.two')
+const buttonThree = document.querySelector('.three')
+const buttonFour = document.querySelector('.four')
+const buttonFive = document.querySelector('.five')
+const buttonSix = document.querySelector('.six')
+const buttonSeven = document.querySelector('.seven')
+const buttonEight = document.querySelector('.eight')
+const buttonNine = document.querySelector('.nine')
+const buttonZero = document.querySelector('.zero')
+
+display.innerHTML = '0';
+
+const eachNumberPress = (button) => {
+    if (display.innerHTML == '0') { display.innerHTML = button.innerHTML }
+    else if (display.innerHTML.length < 8) { display.innerHTML += button.innerHTML }
+
+}
+buttonOne.addEventListener('click', event => { eachNumberPress(buttonOne) })
+buttonTwo.addEventListener('click', event => { eachNumberPress(buttonTwo) })
+buttonThree.addEventListener('click', event => { eachNumberPress(buttonThree) })
+buttonFour.addEventListener('click', event => { eachNumberPress(buttonFour) })
+buttonFive.addEventListener('click', event => { eachNumberPress(buttonFive) })
+buttonSix.addEventListener('click', event => { eachNumberPress(buttonSix) })
+buttonSeven.addEventListener('click', event => { eachNumberPress(buttonSeven) })
+buttonEight.addEventListener('click', event => { eachNumberPress(buttonEight) })
+buttonNine.addEventListener('click', event => { eachNumberPress(buttonNine) })
+buttonZero.addEventListener('click', event => { eachNumberPress(buttonZero) })
