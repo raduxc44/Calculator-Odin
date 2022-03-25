acbb635cdea70ca52d89fec7109189d6ab1fb464
raduxc44 <raduxc44@gmail.com>
2022-03-26 00:23:19 +0200

    Operands building not working yet

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
+
+// First declarations
+
+const numberButtons = Array.from(document.querySelectorAll('.number'));
+const display = document.querySelector('.display-value');
+const buttonOne = document.querySelector('.one')
+const buttonTwo = document.querySelector('.two')
+const buttonThree = document.querySelector('.three')
+const buttonFour = document.querySelector('.four')
+const buttonFive = document.querySelector('.five')
+const buttonSix = document.querySelector('.six')
+const buttonSeven = document.querySelector('.seven')
+const buttonEight = document.querySelector('.eight')
+const buttonNine = document.querySelector('.nine')
+const buttonZero = document.querySelector('.zero')
+const deleteButton = document.querySelector('.delete-butt')
+const addOperator = document.querySelector('.add')
+const subOperator = document.querySelector('.sub')
+const multOperator = document.querySelector('.mult')
+const divOperator = document.querySelector('.div')
+const operators = Array.from(document.querySelectorAll('.operator'))
+
+let firstOperand = '0';
+let secondOperand = '0';
+display.innerHTML = '0';
+
+// Math functions
+
 const add = (a, b) => a + b;
 const substract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
@@ -18,33 +46,74 @@ const operate = (operator, a, b) => {
             break;
     }
 }
-const numberButtons = Array.from(document.querySelectorAll('.number'));
-const display = document.querySelector('.display-value');
-const buttonOne = document.querySelector('.one')
-const buttonTwo = document.querySelector('.two')
-const buttonThree = document.querySelector('.three')
-const buttonFour = document.querySelector('.four')
-const buttonFive = document.querySelector('.five')
-const buttonSix = document.querySelector('.six')
-const buttonSeven = document.querySelector('.seven')
-const buttonEight = document.querySelector('.eight')
-const buttonNine = document.querySelector('.nine')
-const buttonZero = document.querySelector('.zero')
 
-display.innerHTML = '0';
+// Operators functionality
 
-const eachNumberPress = (button) => {
-    if (display.innerHTML == '0') { display.innerHTML = button.innerHTML }
-    else if (display.innerHTML.length < 8) { display.innerHTML += button.innerHTML }
+addOperator.addEventListener('click', event => { operatorPress() })
+
+const operatorPress = (operator) => {
+    if (display.innerHTML != '0') {
+        display.innerHTML = '0';
+    }
+    secondOperandBuilding()
+}
+
+// Building the first operand
+
+const firstOperandBuilding = () => {
+
+    const eachNumberPress = (numButton) => {
+        if (display.innerHTML == '0') {
+            firstOperand = numButton.innerHTML;
+            display.innerHTML = numButton.innerHTML;
+        }
+        else if (display.innerHTML.length < 8) {
+            firstOperand += numButton.innerHTML;
+            display.innerHTML += numButton.innerHTML;
+        }
+    }
+
+    buttonOne.addEventListener('click', event => { eachNumberPress(buttonOne) })
+    buttonTwo.addEventListener('click', event => { eachNumberPress(buttonTwo) })
+    buttonThree.addEventListener('click', event => { eachNumberPress(buttonThree) })
+    buttonFour.addEventListener('click', event => { eachNumberPress(buttonFour) })
+    buttonFive.addEventListener('click', event => { eachNumberPress(buttonFive) })
+    buttonSix.addEventListener('click', event => { eachNumberPress(buttonSix) })
+    buttonSeven.addEventListener('click', event => { eachNumberPress(buttonSeven) })
+    buttonEight.addEventListener('click', event => { eachNumberPress(buttonEight) })
+    buttonNine.addEventListener('click', event => { eachNumberPress(buttonNine) })
+    buttonZero.addEventListener('click', event => { eachNumberPress(buttonZero) })
+    deleteButton.addEventListener('click', event => { display.innerHTML = '0' })
 
 }
-buttonOne.addEventListener('click', event => { eachNumberPress(buttonOne) })
-buttonTwo.addEventListener('click', event => { eachNumberPress(buttonTwo) })
-buttonThree.addEventListener('click', event => { eachNumberPress(buttonThree) })
-buttonFour.addEventListener('click', event => { eachNumberPress(buttonFour) })
-buttonFive.addEventListener('click', event => { eachNumberPress(buttonFive) })
-buttonSix.addEventListener('click', event => { eachNumberPress(buttonSix) })
-buttonSeven.addEventListener('click', event => { eachNumberPress(buttonSeven) })
-buttonEight.addEventListener('click', event => { eachNumberPress(buttonEight) })
-buttonNine.addEventListener('click', event => { eachNumberPress(buttonNine) })
-buttonZero.addEventListener('click', event => { eachNumberPress(buttonZero) })
+firstOperandBuilding()
+
+// Building the 2nd operator
+
+const secondOperandBuilding = () => {
+
+    const eachNumberPress = (numButton) => {
+
+        if (firstOperand != '0' && display.innerHTML == '0') {
+            secondOperand = numButton.innerHTML;
+            display.innerHTML = numButton.innerHTML;
+        }
+        else if (firstOperand != '0' && display.innerHTML.length < 8) {
+            secondOperand += numButton.innerHTML;
+            display.innerHTML += numButton.innerHTML;
+        }
+    }
+
+    buttonOne.addEventListener('click', event => { eachNumberPress(buttonOne) })
+    buttonTwo.addEventListener('click', event => { eachNumberPress(buttonTwo) })
+    buttonThree.addEventListener('click', event => { eachNumberPress(buttonThree) })
+    buttonFour.addEventListener('click', event => { eachNumberPress(buttonFour) })
+    buttonFive.addEventListener('click', event => { eachNumberPress(buttonFive) })
+    buttonSix.addEventListener('click', event => { eachNumberPress(buttonSix) })
+    buttonSeven.addEventListener('click', event => { eachNumberPress(buttonSeven) })
+    buttonEight.addEventListener('click', event => { eachNumberPress(buttonEight) })
+    buttonNine.addEventListener('click', event => { eachNumberPress(buttonNine) })
+    buttonZero.addEventListener('click', event => { eachNumberPress(buttonZero) })
+    deleteButton.addEventListener('click', event => { display.innerHTML = '0' })
+
+}
